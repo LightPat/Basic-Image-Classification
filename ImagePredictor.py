ae452f1c8603174d07688add8a056f966415522a
LightPat <60397186+LightPat@users.noreply.github.com>
2021-12-12 15:50:19 -0500

    Small Update for Pres

--- ImagePredictor.py
+++ ImagePredictor.py
@@ -7,6 +7,7 @@ Created on Thu Dec  9 12:37:52 2021
 
 import cv2
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from ImagePreprocessing import preprocess_image
 
@@ -23,11 +24,23 @@ def decode_predictions(predictionArray):
     return decoded
 
 if __name__ == "__main__":
-    H5FileName = r"G:\Python Projects\AlienVsPredator CNN\Run 2_97 Percent 40 Epochs\After Training CNN.h5"
-    imageToPredict = r"G:\Python Projects\AlienVsPredator CNN\AlienVsPredatorDataset\test\predator\3.jpg"
+    H5FileName = r"C:\Users\Patrick Seeman\Desktop\Best CNN.h5"
+    imageToPredict = r"C:\Users\Patrick Seeman\Desktop\predator.jfif"
+    imageToPredict2 = r"C:\Users\Patrick Seeman\Desktop\alien.jpeg"
     
+    # First Image
     cnn = load_model(H5FileName)
     img = preprocess_image(imageToPredict)
+    img = np.expand_dims(img, axis=0)
+    prediction = cnn.predict(img)
+    decodedPrediction = decode_predictions(prediction)
+    print("DECIMAL REPRESENTATION\nAlien:", decodedPrediction["alien"], "\nPredator:", decodedPrediction["predator"])
+    
+    print()
+    
+    # Second Image
+    cnn = load_model(H5FileName)
+    img = preprocess_image(imageToPredict2)
     img = np.expand_dims(img, axis=0)
     prediction = cnn.predict(img)
     decodedPrediction = decode_predictions(prediction)
